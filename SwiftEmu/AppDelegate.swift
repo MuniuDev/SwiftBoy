10cb06e7c52679b30f98b316368af936c5b2425d
Michal Majczak <michal.majczak92@gmail.com>
2015-09-08 19:20:45 +0200

    Initial Commit

---
+++ SwiftEmu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftEmu
+//
+//  Created by Michal Majczak on 08.09.2015.
+//  Copyright (c) 2015 Michal Majczak. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftEmu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftEmu
+//
+//  Created by Michal Majczak on 08.09.2015.
+//  Copyright (c) 2015 Michal Majczak. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
